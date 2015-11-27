8ec5cd14aa921ee9add72a33e3377211bd22ce9d
Ankush Kalkote <ankushkalkote@Ankushs-MacBook-Air.local>
2015-11-27 12:53:36 +0530

    Use postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151126141826) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "areas", force: :cascade do |t|
     t.string   "area_name"
     t.datetime "created_at", null: false
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 20151126141826) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "comments", ["ticket_id"], name: "index_comments_on_ticket_id"
+  add_index "comments", ["ticket_id"], name: "index_comments_on_ticket_id", using: :btree
 
   create_table "tickets", force: :cascade do |t|
     t.string   "customer_email"
@@ -57,7 +60,8 @@ ActiveRecord::Schema.define(version: 20151126141826) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "tickets"
 end
